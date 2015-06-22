a98f76cdde0cd60f969766cf80c9a4860bfc65c5
Brijesh Patel <Brijeshrpatel9@users.noreply.github.com>
2015-06-21 20:27:56 -0400

    Update MainJava.java

--- src/main/java/org/finra/datagenerator/MainJava.java
+++ src/main/java/org/finra/datagenerator/MainJava.java
@@ -14,7 +14,7 @@ public class MainJava implements Serializable {
 
         // You can define your own file "file.txt" in your directory with first line 1000000 and second line 100
 
-        // Read imput data
+        // Read imput data from file using ClassLoader and InputStream
         ClassLoader classLoader = MainJava.class.getClassLoader();
 
         InputStream is = new FileInputStream(classLoader.getResource("file/input.txt").getFile());
