4928ef75210dc6b48e172a5eea9ecd0d75e86fd5
Brijesh Patel <Brijeshrpatel9@users.noreply.github.com>
2015-07-25 23:01:49 -0400

    Update SparkDistributor.scala

--- src/main/java/org/finra/datagenerator/SparkDistributor.scala
+++ src/main/java/org/finra/datagenerator/SparkDistributor.scala
@@ -50,7 +50,8 @@ class SparkDistributor extends SearchDistributor with java.io.Serializable {
 
     println("Frontier list size = " + (frontierList.size() - 1))
 
-    // You can run program on multiple JVM by mentioning masterURL 
+    // You can run program on multiple JVM by mentioning masterURL
+    // Your MasterURL
     val conf: SparkConf = new SparkConf().setMaster("local[5]").setAppName("DG Spark Example")
 
     val mySparkContext: SparkContext = new SparkContext(conf)
